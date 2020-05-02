3d4f9585ad1d4e4099a361b10b377b46a78cfaa3
Daniel Perego <perego86@gmail.com>
2020-05-02 19:14:54 -0300

    Add and Removed method implemented

---
+++ build/precache-manifest.7460759c48da5ed26476fc0252d1fee8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "144071e04ae92f00f582355aa48700e7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b50cb1f7cdd9fd5c1e4e",
+    "url": "/static/css/main.0b137c96.chunk.css"
+  },
+  {
+    "revision": "eb915607d0f4596d5150",
+    "url": "/static/js/2.fd844d92.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fd844d92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b50cb1f7cdd9fd5c1e4e",
+    "url": "/static/js/main.6214f9f0.chunk.js"
+  },
+  {
+    "revision": "b05506d192fa13a4b14c",
+    "url": "/static/js/runtime-main.cb08f1e8.js"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -1,30 +1,66 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
 import "./styles.css";
+import api from "./services/api";
+
 
 function App() {
+
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then( response => {
+      setRepositories(response.data);
+    })
+  },[]);
+
   async function handleAddRepository() {
-    // TODO
+  
+    const response = await api.post('/repositories', {
+    url: "https://github.com/josepholiveira",
+    title: "Desafio ReactJS",
+    techs: ["React", "Node.js"]});
+
+    const test = response.data;
+
+    setRepositories([...repositories, test]);
+
+    
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    let index = repositories.findIndex(s => s.id === id);
+
+    if (index < 0)   return;
+
+    const response  = await api.delete('/repositories/' + id);
+
+    if (response.status !== 204) return;
+    
+    repositories.splice(index,1);
+
+    setRepositories([...repositories]);
   }
 
   return (
-    <div>
+    <>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+       
+          {repositories.map(repository => 
+              <li key={repository.id}>{repository.title}
+                <button key={repository.id} onClick={() => handleRemoveRepository(repository.id)}>
+                  Remover
+                </button>
+              </li>
+              
+              )}
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        
+       
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
-    </div>
+    </>
   );
 }
 
